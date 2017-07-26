4fc87eb8a5a0f1a636c91c5767064f903b852d79
greenteamuimui <siuding0601@gmail.com>
2017-07-25 23:33:34 -0700

    Add more to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,9 @@ u7 = User.create(username: "Zelda", email:"zelda@gmail.com", password:"123456")
 u8 = User.create(username: "Johnny", email:"johnny@gmail.com", password:"123456")
 
 
-c1 = City.create(name:"San Francisco", photo_url:"http://res.cloudinary.com/greenteamuimui/image/upload/v1500509423/sf_lzgrsr.jpg")
+c1 = City.create(name:"San Francisco", photo_url:"http://res.cloudinary.com/greenteamuimui/image/upload/v1501043971/sf1_duv0ry.jpg")
 c2 = City.create(name:"New Orleans", photo_url:"http://res.cloudinary.com/greenteamuimui/image/upload/c_scale,h_1800/v1500509703/nola_eirw5u.png")
-c3 = City.create(name:"Hong Kong", photo_url:"http://res.cloudinary.com/greenteamuimui/image/upload/v1500509813/hk_wjtwej.jpg")
+c3 = City.create(name:"Hong Kong", photo_url:"http://res.cloudinary.com/greenteamuimui/image/upload/v1501050713/hk1_sj3tqx.jpg")
 c4 = City.create(name:"London", photo_url:"http://res.cloudinary.com/greenteamuimui/image/upload/v1500510203/london_clojc4.jpg")
 c5 = City.create(name:"Stockholm", photo_url:"http://res.cloudinary.com/greenteamuimui/image/upload/v1500510371/stockholm1_wvs9ib.jpg")
 
